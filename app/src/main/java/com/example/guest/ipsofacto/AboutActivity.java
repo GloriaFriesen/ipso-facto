6fb605ab2957cbbb850b03de2137c0c3df5e0225
Gloria Friesen <gloriafriesen@gmail.com>
2017-05-26 12:00:24 -0700

    add content to about activity, setOnClickListener to start Contact activity when developer name clicked

--- app/src/main/java/com/example/guest/ipsofacto/AboutActivity.java
+++ app/src/main/java/com/example/guest/ipsofacto/AboutActivity.java
@@ -1,13 +1,34 @@
 package com.example.guest.ipsofacto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class AboutActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.startContactFromAbout)
+    TextView mStartContactFromAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ButterKnife.bind(this);
+
+        mStartContactFromAbout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mStartContactFromAbout) {
+            Intent intent = new Intent(AboutActivity.this, ContactActivity.class);
+            startActivity(intent);
+        }
     }
 }
