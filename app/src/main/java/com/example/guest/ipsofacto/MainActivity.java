8d0139e0812c0b4aa5aef9fb8487b3a93afca43f
Gloria Friesen <gloriafriesen@gmail.com>
2017-05-26 16:12:43 -0700

    add AutoCompleteTextView to MainActivity, user can search from list of 50 states with autocomplete functionality

--- app/src/main/java/com/example/guest/ipsofacto/MainActivity.java
+++ app/src/main/java/com/example/guest/ipsofacto/MainActivity.java
@@ -4,19 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String [] states = new String[] {
+            "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming", "District of Columbia"
+    };
+
     @Bind(R.id.submitLocationButton)
     Button mSubmitLocationButton;
     @Bind(R.id.locationEditText)
     EditText mLocationEditText;
     @Bind(R.id.startAboutActivity) Button mStartAboutActivity;
     @Bind(R.id.startContactActivity) Button mStartContactActivity;
+    @Bind(R.id.stateTextView) AutoCompleteTextView mStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSubmitLocationButton.setOnClickListener(this);
         mStartAboutActivity.setOnClickListener(this);
         mStartContactActivity.setOnClickListener(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, states);
+        mStateTextView.setAdapter(adapter);
     }
 
     @Override
