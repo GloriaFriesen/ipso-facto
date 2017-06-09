fd95ce612c95f66ea513aaec60018eab7d6f586f
Gloria Friesen <gloriafriesen@gmail.com>
2017-06-09 14:22:48 -0700

    add registerButton click listener, sends user to CreateAccountActivity

--- app/src/main/java/com/example/guest/ipsofacto/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/guest/ipsofacto/ui/CreateAccountActivity.java
@@ -1,15 +1,27 @@
 package com.example.guest.ipsofacto.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.guest.ipsofacto.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
     }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
 }
