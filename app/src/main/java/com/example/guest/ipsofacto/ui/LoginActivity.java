22acde14b7dfd3fdeac92df2a3f02afab1ce1d39
Gloria Friesen <gloriafriesen@gmail.com>
2017-06-10 17:17:31 -0700

    add login and form validation to LoginActivity

--- app/src/main/java/com/example/guest/ipsofacto/ui/LoginActivity.java
+++ app/src/main/java/com/example/guest/ipsofacto/ui/LoginActivity.java
@@ -1,26 +1,42 @@
 package com.example.guest.ipsofacto.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.guest.ipsofacto.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
     @Bind(R.id.registerButton) Button mRegisterButton;
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         ButterKnife.bind(this);
+
         mRegisterButton.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -30,5 +46,48 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(intent);
             finish();
         }
+        if (view == mLoginButton) {
+            loginWithPassword();
+        }
+    }
+
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+
+        boolean isEmailValid = checkEmail(email);
+        boolean isPasswordValid = checkPassword(password);
+
+        if (isEmailValid && isPasswordValid) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("Login", "signInWithEmail:onComplete:" + task.isSuccessful());
+                            if (!task.isSuccessful()) {
+                                Log.w("Login", "signInWithEmail", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private boolean checkEmail(String email) {
+        if (email.equals("")) {
+            mEmailEditText.setError("Email, please.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkPassword(String password) {
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password, s'il vous plait.");
+            return false;
+        }
+        return true;
     }
 }
