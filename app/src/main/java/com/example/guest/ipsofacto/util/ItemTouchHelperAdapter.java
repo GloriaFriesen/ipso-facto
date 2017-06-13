5e9825167e093b7ffb0b76791e4ded7cf414af28
Gloria Friesen <gloriafriesen@gmail.com>
2017-06-12 17:41:53 -0700

    add ItemTouchHelperAdapter and OnStartDragListener interfaces, add SimpleItemTouchCallback class

---
+++ app/src/main/java/com/example/guest/ipsofacto/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,7 @@
+package com.example.guest.ipsofacto.util;
+
+
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
